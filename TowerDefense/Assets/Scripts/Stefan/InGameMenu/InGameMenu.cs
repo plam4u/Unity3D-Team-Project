3b2dc8ab35946f90a3726c2c906219940d0cd93c
sparrowbg <sparrow@data.bg>
2014-11-20 10:18:08 +0200

    Update Na InGameMenu.cs

--- TowerDefense/Assets/Scripts/Stefan/InGameMenu/InGameMenu.cs
+++ TowerDefense/Assets/Scripts/Stefan/InGameMenu/InGameMenu.cs
@@ -20,60 +20,190 @@ public class InGameMenu : MonoBehaviour
     public Texture2D menuItem;
     public Texture2D menuBg;
 
-    List<GUI> guiList = new List<GUI>();
+
 
     void Awake()
     {
         menuPosX = Screen.width - menuWidth;
-        menuPosY = Screen.height - menuHeght;        
-        
+        menuPosY = Screen.height - menuHeght;
+
     }
 
     void OnGUI()
     {
         GUI.skin = inGameSkin;
-        All();
+        InGame();
     }
+
+    private void InGame()
+    {
+
+        GUI.BeginGroup(new Rect(menuPosX, menuPosY, menuWidth, menuHeght), menuBg);
+
+        if (GUI.Button(new Rect(4, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 1");
+        }
+
+        if (GUI.Button(new Rect(56, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 2");
+        }
+
+        if (GUI.Button(new Rect(108, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 3");
+        }
+
+        if (GUI.Button(new Rect(160, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 4");
+        }
+
+        if (GUI.Button(new Rect(4, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 5");
+        }
+
+        if (GUI.Button(new Rect(56, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 6");
+        }
+
+        if (GUI.Button(new Rect(108, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 7");
+        }
+
+        if (GUI.Button(new Rect(160, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 8");
+        }
+
+        if (GUI.Button(new Rect(4, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 9");
+        }
+
+        if (GUI.Button(new Rect(56, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 10");
+        }
+
+        if (GUI.Button(new Rect(108, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 11");
+        }
+
+        if (GUI.Button(new Rect(160, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 12");
+        }
+
+        GUI.EndGroup();
+    }
+
+
+
+
 
     private void All()
     {
         GUI.BeginGroup(new Rect(menuPosX, menuPosY, menuWidth, menuHeght), menuBg);
-            
-            GUI.Button(new Rect(4, 4, 48, 48), menuItem);
-            GUI.Button(new Rect(56, 4, 48, 48), menuItem);
-            GUI.Button(new Rect(108, 4, 48, 48), menuItem);
-            GUI.Button(new Rect(160, 4, 48, 48), menuItem);
 
-            GUI.Button(new Rect(4, 56, 48, 48), menuItem);
-            GUI.Button(new Rect(56, 56, 48, 48), menuItem);
-            GUI.Button(new Rect(108, 56, 48, 48), menuItem);
-            GUI.Button(new Rect(160, 56, 48, 48), menuItem);
+        GUI.Button(new Rect(4, 4, 48, 48), menuItem);
+        GUI.Button(new Rect(56, 4, 48, 48), menuItem);
+        GUI.Button(new Rect(108, 4, 48, 48), menuItem);
+        GUI.Button(new Rect(160, 4, 48, 48), menuItem);
 
-            GUI.Button(new Rect(4, 108, 48, 48), menuItem);
-            GUI.Button(new Rect(56, 108, 48, 48), menuItem);
-            GUI.Button(new Rect(108, 108, 48, 48), menuItem);
-            GUI.Button(new Rect(160, 108, 48, 48), menuItem);
-            
+        GUI.Button(new Rect(4, 56, 48, 48), menuItem);
+        GUI.Button(new Rect(56, 56, 48, 48), menuItem);
+        GUI.Button(new Rect(108, 56, 48, 48), menuItem);
+        GUI.Button(new Rect(160, 56, 48, 48), menuItem);
+
+        GUI.Button(new Rect(4, 108, 48, 48), menuItem);
+        GUI.Button(new Rect(56, 108, 48, 48), menuItem);
+        GUI.Button(new Rect(108, 108, 48, 48), menuItem);
+        GUI.Button(new Rect(160, 108, 48, 48), menuItem);
+
         GUI.EndGroup();
     }
 
     private void Half()
     {
         GUI.BeginGroup(new Rect(menuPosX, menuPosY, menuWidth, menuHeght));
-            GUI.Button(new Rect(10, 10, 40, 40), menuItem);
-            GUI.Button(new Rect(60, 10, 40, 40), menuItem);
-            GUI.Button(new Rect(110, 10, 40, 40), menuItem);
-            GUI.Button(new Rect(10, 60, 40, 40), menuItem);        
+        GUI.Button(new Rect(10, 10, 40, 40), menuItem);
+        GUI.Button(new Rect(60, 10, 40, 40), menuItem);
+        GUI.Button(new Rect(110, 10, 40, 40), menuItem);
+        GUI.Button(new Rect(10, 60, 40, 40), menuItem);
         GUI.EndGroup();
     }
 
     private void TestMenu()
     {
 
-        GUI.BeginGroup(new Rect(menuPosX, menuPosY, menuWidth, menuHeght));
+        GUI.BeginGroup(new Rect(menuPosX, menuPosY, menuWidth, menuHeght), menuBg);
 
-        
-        
+        if (GUI.Button(new Rect(4, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 1");
+        }
+
+        if (GUI.Button(new Rect(56, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 2");
+        }
+
+        if (GUI.Button(new Rect(108, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 3");
+        }
+
+        if (GUI.Button(new Rect(160, 4, 48, 48), menuItem))
+        {
+            Debug.Log("Button 4");
+        }
+
+        if (GUI.Button(new Rect(4, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 5");
+        }
+
+        if (GUI.Button(new Rect(56, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 6");
+        }
+
+        if (GUI.Button(new Rect(108, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 7");
+        }
+
+        if (GUI.Button(new Rect(160, 56, 48, 48), menuItem))
+        {
+            Debug.Log("Button 8");
+        }
+
+        if (GUI.Button(new Rect(4, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 9");
+        }
+
+        if (GUI.Button(new Rect(56, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 10");
+        }
+
+        if (GUI.Button(new Rect(108, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 11");
+        }
+
+        if (GUI.Button(new Rect(160, 108, 48, 48), menuItem))
+        {
+            Debug.Log("Button 12");
+        }
+
         GUI.EndGroup();
     }
 
