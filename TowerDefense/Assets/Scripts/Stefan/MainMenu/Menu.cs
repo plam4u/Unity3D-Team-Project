b9cb0f69551e845c599c194e70f81e29f2be2cb6
sparrowbg <sparrow@data.bg>
2014-11-20 11:58:35 +0200

    Veche Ima scena za main menu. Ima prefab main menu koito trbqbva da se adva na vsqka scena. No shte pravq oshte update po nego

--- TowerDefense/Assets/Scripts/Stefan/MainMenu/Menu.cs
+++ TowerDefense/Assets/Scripts/Stefan/MainMenu/Menu.cs
@@ -4,24 +4,54 @@ using System.Collections;
 public class Menu : MonoBehaviour
 {
 
-    public bool isQuit;
+    public GUISkin menuSkin = null;
+
+    public Texture2D menuItem;
+    public Texture2D menuBg;
+
+    float menuWidth = 400.0f;
+    float menuHeght = 600.0f;
+    float menuPosX;
+    float menuPosY;
+
+
+
     public Color mainColor;
 
-    void Start()
+    void Awake()
     {
-        mainColor = renderer.material.color;
-    }
-
-    void OnMouseOver()
-    {
-        renderer.material.color = Color.gray;
+        menuPosX = (Screen.width / 2) - (menuWidth / 2);
+        menuPosY = (Screen.height / 2) - (menuHeght / 2);
 
     }
 
-    void OnMouseExit()
-    {
-        renderer.material.color = mainColor;
-    }
 
+    void OnGUI()
+    {
+
+        GUI.skin = menuSkin;
+        GUI.BeginGroup(new Rect(menuPosX, menuPosY, menuWidth, menuHeght), menuBg);
+        GUI.Box(new Rect(0, 30, menuWidth, menuHeght), "TOWER DEFENCE");
+        GUI.BeginGroup(new Rect(100, 200, 200, 500));
+
+        if(GUI.Button(new Rect(0, 0, 200, 50), "Start Game"))
+        {
+            Debug.Log("Application Load Level");
+        }
+
+        if(GUI.Button(new Rect(0, 60, 200, 50), "Options"))
+        {
+            Debug.Log("Application Load Option");
+        }
+
+        if (GUI.Button(new Rect(0, 120, 200, 50), "Exit Game"))
+        {
+            Debug.Log("Application Quit");
+        }
+        
+        GUI.EndGroup();
+        GUI.EndGroup();
+
+    }
 
 }
