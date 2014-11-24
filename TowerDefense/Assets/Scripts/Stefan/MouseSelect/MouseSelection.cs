31351577f58b93429cb5a88a1552e36e272041f7
sparrowbg <sparrow@data.bg>
2014-11-24 12:06:57 +0200

    Added simple scene L1 with mouse control to build

---
+++ TowerDefense/Assets/Scripts/Stefan/MouseSelect/MouseSelection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseSelection : MonoBehaviour
+{
+
+    private bool selected = false;
+
+    private GameObject prevGameObject;
+
+    private GameObject currentGameObject;
+
+    private LayerMask layerMask = 1 << 8;
+
+    private bool buildMode = true;
+    
+    void Update()
+    {
+
+        if(buildMode)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100, layerMask))
+            {
+                hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
+        }
+
+    }
+}
