7fba7bab4d0d5725586641bf8be7e8b93b503d4f
eqinox <eqinox@abv.bg>
2014-11-27 16:13:59 +0200

    start implementing EnemyEngine.cs and Paths

--- TowerDefense/Assets/Scripts/Vasko/EnemyEngine.cs
+++ TowerDefense/Assets/Scripts/Vasko/EnemyEngine.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using System.Threading;
 
 namespace Assets.Scripts.Vasko
 {
     class EnemyEngine : MonoBehaviour
     {
         public GameObject enemy;
+        public GameObject startPos;
+        public List<GameObject> path;
+        public int enemies = 10;
+        public float time = 10;
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            //Thread.Sleep(500);
+
+        }
+
+        public bool MeasureTime(float time)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
